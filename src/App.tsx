bd9943123b4376bb738abe78d65459826826dcda
noda-dev <kota13011726@icloud.com>
2020-04-11 22:48:49 +0900

    テキスト付き画像コンポーネント作成

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Greeting from './components/Greeting';
+import PhotoText from './components/PhotoText';
 
 type Props = {}
 
@@ -16,9 +16,9 @@ class App extends React.Component<Props, State> {
   render() {
     return(
       <div>
-        <Greeting message="おはよう"/>
-        <Greeting message="こんにちは"/>
-        <Greeting message="こんばんは"/>
+        <PhotoText label="カニンヘンダックス1" url="https://www.min-inuzukan.com/images/detailMain_kaninchen-dachshund.png?20200203_l"/>
+        <PhotoText label="カニンヘンダックス2" url="https://www.pretty-wan.com/wp-content/uploads/IMG_6037.jpg"/>
+        <PhotoText label="カニンヘンダックス3" url="https://d1k5eswq61k0il.cloudfront.net/uploads/item/image1/2730/s560x420_1046_p1412250341.jpg"/>
       </div>
     )
   }
